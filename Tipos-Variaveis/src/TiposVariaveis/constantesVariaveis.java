b4c451ac1c42ece5c2fc92cd6f9c254bd8fd3e37
neg0uiu <willianx2002.ws@gmail.com>
2022-10-01 11:07:36 -0300

    tipo String adicionado

--- Tipos-Variaveis/src/TiposVariaveis/constantesVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis/constantesVariaveis.java
@@ -13,10 +13,12 @@ public class constantesVariaveis {
             //exemplo:
             final int INTEIRO = 7;
 
+        //Estudar o tipo String que eh texto
+        String meuNome = "willian";
 
         System.out.println("variavel: " + inteiro);
         System.out.println("CONSTANTE: " + INTEIRO);
-
+        System.out.println("meu nome eh: " + meuNome);
 
     }
 }

27f7aca060030dbb06bc6afe78fb1a98188212a6
neg0uiu <willianx2002.ws@gmail.com>
2022-10-01 11:02:18 -0300

    CONSTANTES E VARIAVEIS

---
+++ Tipos-Variaveis/src/TiposVariaveis/constantesVariaveis.java
@@ -0,0 +1,22 @@
+package tiposvariaveis;
+
+public class constantesVariaveis {
+    public static void main(String[] args) {
+        
+        //isso eh uma VARIAVEL
+        // (tipo) (nome) = (valor);
+            //exemplo:
+            int inteiro = 5;
+
+        //isso eh uma CONSTANTE (sempre com "final" antes do tipo e o nome com caixa alta)
+        // final (tipo) (NOME) = (valor);
+            //exemplo:
+            final int INTEIRO = 7;
+
+
+        System.out.println("variavel: " + inteiro);
+        System.out.println("CONSTANTE: " + INTEIRO);
+
+
+    }
+}
